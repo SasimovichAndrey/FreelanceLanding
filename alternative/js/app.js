d6b19a2bb781cb7d2c138be83095d9e8b6218e46
Sinitsin <ballack.tyta@gmail.com>
2017-10-04 14:48:56 +0300

    add member tabs resizing

--- alternative/js/app.js
+++ alternative/js/app.js
@@ -2,17 +2,46 @@ $(function() {
   "use strict";
   $('.about-member-info').last().show().animate({
     width: '94%'
+  },{
+    complete: setAboutMemberHeaderHeightFunc
   });
+  
   $('.about-member-header').click(function() {
-    $(this).siblings(".about-member-info").animate({
-        width: 0
-      });
+    var clickedHeader = this;
     $(this)
-      .next().show().animate({
-        width: '94%'
+        .next()
+
+        .show()
+        .css('height', ($(clickedHeader).css('height')))
+        .animate({
+          width: '94%',
+        },
+        {
+          queue: true,
+          duration:500,
+          complete: setAboutMemberHeaderHeightFunc
+        })
+        .siblings(".about-member-info")
+        .css('height', ($(this).css('height')))
+        .animate({
+          width: 0
+        }, {
+          duration: 200,
+          //complete: setAboutMemberHeaderHeightFunc
+        });
+        
       })
-      
+  ;
+  
 
-  });
+  function setAboutMemberHeaderHeightFunc() {
 
+        $(this).css('height', 'auto');
+        var maxHeight;
+        $('.about-member-info')
+            .each(function() {
+              maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
+            });
+        $('.about-member-header').css('height', maxHeight +'px');
+    }
 });
