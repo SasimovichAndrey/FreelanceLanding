dfddd8fa86c2fc19b6f55192c1786c0bee9177e6
Sinitsin <ballack.tyta@gmail.com>
2017-10-09 14:50:33 +0300

    change team section

--- alternative/js/app.js
+++ alternative/js/app.js
@@ -1,53 +1,7 @@
 $(function() {
   "use strict";
 
-  /* Members slider */
-  $('.about-member-info').last().show().animate({
-    width: '94%'
-  },{
-    complete: setAboutMemberHeaderHeightFunc
-  });
-  
-  $('.about-member-header').click(function() {
-    var clickedHeader = this;
-    $(this)
-        .next()
-
-        .show()
-        .css('height', ($(clickedHeader).css('height')))
-        .animate({
-          width: '94%',
-        },
-        {
-          queue: true,
-          duration:500,
-          complete: setAboutMemberHeaderHeightFunc
-        })
-        .siblings(".about-member-info")
-        .css('height', ($(this).css('height')))
-        .animate({
-          width: 0
-        }, {
-          duration: 200,
-          //complete: setAboutMemberHeaderHeightFunc
-        });
-        
-      })
-  ;
-  
-
-  function setAboutMemberHeaderHeightFunc() {
-
-        $(this).css('height', 'auto');
-        var maxHeight;
-        $('.about-member-info')
-            .each(function() {
-              maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
-            });
-        $('.about-member-header').css('height', maxHeight +'px');
-        hideLoader();
-    }
-
+    window.setTimeout(function(){hideLoader()}, 1000);
 
     var loaderHided = false;
     /* Loader*/
